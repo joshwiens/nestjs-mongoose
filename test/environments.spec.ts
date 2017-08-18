c7464a3dbe011861952ef9480eae1244df79f478
Joshua Wiens <joshuaw@easymetrics.com>
2017-08-17 20:11:24 -0500

    test: Add environment spec

---
+++ test/environments.spec.ts
@@ -0,0 +1,19 @@
+import { Environments } from '../src/shared/environments';
+
+describe('Environment', () => {
+  test('getName() should return the test env', () => {
+    expect(Environments.getEnv()).toBe('test');
+  });
+
+  test('isTest() should be true', () => {
+    expect(Environments.isTest()).toBeTruthy();
+  });
+
+  test('isDevelopment() should be false', () => {
+    expect(Environments.isDev()).toBeFalsy();
+  });
+
+  test('isProduction() should be false', () => {
+    expect(Environments.isProd()).toBeFalsy();
+  });
+});
