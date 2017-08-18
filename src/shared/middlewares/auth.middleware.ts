6b0a9acaf3dcaaa0f7699b3b244f0518bf5e1519
Joshua Wiens <joshuaw@easymetrics.com>
2017-08-18 08:36:14 -0500

    fix: Correct return type for auth middleware

--- src/shared/middlewares/auth.middleware.ts
+++ src/shared/middlewares/auth.middleware.ts
@@ -1,34 +1,23 @@
 import { Middleware, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
-import * as jwt from 'express-jwt';
 
+const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
-
-import { UnauthorizedException } from '../exceptions';
 
 @Middleware()
 export class AuthMiddleware implements NestMiddleware {
-  public resolve() {
-    return (req: Request, res: Response, next: NextFunction) => {
-      const authorizationHeaders = req.headers['authorization'] as string;
-      if (!authorizationHeaders) {
-        throw new UnauthorizedException('No authorization token on request');
-      }
-      const checkJwt = jwt({
-        secret: jwksRsa.expressJwtSecret({
-          cache: false,
-          rateLimit: true,
-          jwksRequestsPerMinute: 60,
-          jwksUri: process.env.AUTH_JWKS_URL,
-        }),
-        audience: process.env.AUTH_AUDIENCE,
-        issuer: process.env.AUTH_ISSUER,
-        algorithms: ['RS256'],
-      });
-      if (!checkJwt) {
-        throw new UnauthorizedException('Invalid Token');
-      }
-      next();
-    };
+  public resolve(): (req: Request, res: Response, next: NextFunction) => void {
+    const secret = jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 60,
+      jwksUri: process.env.AUTH_JWKS_URL,
+    });
+    return jwt({
+      secret: secret,
+      audience: process.env.AUTH_AUDIENCE,
+      issuer: process.env.AUTH_ISSUER,
+      algorithms: ['RS256'],
+    });
   }
 }
